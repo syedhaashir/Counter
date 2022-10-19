8ca3b344da87b0833a9646f562c169724eb884df
syedhaashir <hashir.hali@gmail.com>
2022-10-19 20:42:33 +0500

    up

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ function App() {
 
   return (
     <>
+      <div className='head'>
       <h1 className='heading'>Counter Value {counter}</h1>
+      </div>
       <div className='button'>
-      <button onClick={() => setcounter(counter + 1)} className="button1">Increment</button>
-      <button onClick={() => setcounter(counter - 1)} className="button2">Decrement</button>
+      <button onClick={() => setcounter(counter + 1)} className="button-56">Increment</button>
+      <button onClick={() => setcounter(counter - 1)} className="button-56">Decrement</button>
       {/* <button onClick={() => counter()}>Reset</button> */}
       </div>
       <div className="room">the room is lit</div>

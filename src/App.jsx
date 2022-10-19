9b53cb604fb0c1cf8d4867858d00303383b9bd7a
Tazmeenilsa <tazmeenilsa@gmail.com>
2022-10-19 23:18:13 +0500

    addd

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import { useState } from 'react';
 function App() {
   const [counter, setcounter] = useState(0)
   const [isLit, setLit] = useState(true);
-
+  const toggle = () => {
+    setLit(!isLit)
+  }
   return (
     <>
       <div className='head'>
-      <h1 className='heading'>Counter Value {counter}</h1>
+        <h1 className='heading'>Counter Value {counter}</h1>
+        <button onClick={toggle}> Chaprasi Hashir</button>
       </div>
-      <div className='button'>
-      <button onClick={() => setcounter(counter + 1)} className="button-56">Increment</button>
-      <button onClick={() => setcounter(counter - 1)} className="button-56">Decrement</button>
-      {/* <button onClick={() => counter()}>Reset</button> */}
+      <div className={(isLit) ? 'light' : 'dark'}>
+        <button onClick={() => setcounter(counter + 1)} className="button-56">Increment</button>
+        <button onClick={() => setcounter(counter - 1)} className="button-56">Decrement</button>
+        {/* <button onClick={() => counter()}>Reset</button> */}
       </div>
-      <div className="room">the room is lit</div>
+      <div className="room">{(isLit) ? 'Main Gandu Hoon' : 'Main Lora Hoon'}</div>
     </>
   );
 }
